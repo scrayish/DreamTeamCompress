8ebcf8a9681e519b1720fee89f3348466db63303
Daniels Pipurs <danyo911@inbox.lv>
2020-02-26 10:44:46 +0200

    Huffman and HuffmanNode

---
+++ Huffman.java
@@ -0,0 +1,12 @@
+package PACKAGE_NAME;
+
+public class Huffman {
+
+    public static class HuffmanNode {
+        int data;
+        character char;
+        left HuffmanNode;
+        right HuffmanNode;
+    }
+
+}
